08c4c026fdc76049820882a78b7bf7bc1218ecc7
Arne Boon <arneboon@gmail.com>
2016-12-30 20:39:25 +0100

    project page loads [portfolio] shortcake

---
+++ template-parts/home-projects.php
@@ -0,0 +1,26 @@
+<?php
+$page_id = intval( get_option( 'home_projects_content' ) );
+
+if ( 0 == $page_id ) {
+	return;
+}
+?>
+
+<a name="home-section-projects"></a>
+
+<div class="home-section home-download-section">
+	<div class="container">
+		<?php
+		// Title
+		$title = get_the_title( $page_id );
+
+		if ( !empty( $title ) ) {
+			echo '<h2>' . $title . '</h2>';
+		}
+		?>
+
+		<!--Load [portfolio] shortcode-->
+		<?php marvy_page_content_by_id( $page_id, 'the_content' ); ?>
+
+	</div>
+</div>
