3da9deba99aeb7fd847d205af202bd780f7ec42c
Arne Boon <arneboon@gmail.com>
2016-12-30 20:35:51 +0100

    add two columns to single page (todo: create jackpack-portfolio-content-single.php file instead of changing the main content-single file)

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -13,16 +13,25 @@
 			<?php
 		}
 		?>
-
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
 	<footer class="entry-footer">
-		<?php marvy_entry_list_footer(); ?>
+		<!--?php marvy_entry_list_footer(); ?-->
 	</footer>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
+	<div class="entry-content grid">
+		<div class="grid-cell sm-grid-1-1 md-grid-1-2">
+			<p>
+				<?php the_content(); ?>
+			</p>
+		</div>
+		<div class="grid-cell sm-grid-1-1 md-grid-1-2">
+			<p>
+				<?php the_content(); ?>
+			</p>
+		</div>
+
 		<?php
 		wp_link_pages( array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'marvy' ),
