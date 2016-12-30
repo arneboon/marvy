cc88827f77d198d8f3773d1bc3edda0398434f22
Arne Boon <arneboon@gmail.com>
2016-12-30 20:37:21 +0100

    add title and contact anchor (not currently used)

--- template-parts/home-callout.php
+++ template-parts/home-callout.php
@@ -6,9 +6,19 @@ if ( 0 == $page_id ) {
 }
 ?>
 
-<div class="home-section home-download-section">
+<a name="home-section-contact"></a>
+
+<div class="home-section home-download-section home-bg-grey">
 	<div class="container">
 		<div class="home-callout">
+			<?php
+			// Title
+			$title = get_the_title( $page_id );
+
+			if ( !empty( $title ) ) {
+				echo '<h2>' . $title . '</h2>';
+			}
+			?>
 			<?php marvy_page_content_by_id( $page_id, 'the_content' ); ?>
 		</div>
 	</div>
