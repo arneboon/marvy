e66dd1313059a39a8c40ab86ac5fd5eca899c244
Arne Boon <arneboon@gmail.com>
2017-01-01 22:06:00 +0100

    implement youtube, content, intro, gallery. now work on page layout

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -8,29 +8,46 @@
 	<header class="entry-header">
 		<?php if ( has_post_thumbnail() ) { ?>
 			<div class="entry-media">
-				<?php the_post_thumbnail( 'marvy-large-thumb' ); ?>
+				<!--?php the_post_thumbnail( 'marvy-full-width-thumb' ); ?-->
 			</div>
 			<?php
 		}
 		?>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h6>
+			<?php the_date('Y'); ?>
+			&nbsp;&nbsp;
+			<?php the_field('location'); ?>
+		</h6>
 	</header><!-- .entry-header -->
 
-	<footer class="entry-footer">
-		<!--?php marvy_entry_list_footer(); ?-->
-	</footer>
-
 	<div class="entry-content grid">
-		<div class="grid-cell sm-grid-1-1 md-grid-1-2">
-			<p>
-				<?php the_content(); ?>
-			</p>
+		<div class="grid-cell">
+
+			<p><?php the_field('introduction'); ?></p>
+			<p><?php the_content(); ?></p>
+
+			<?php
+				//https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+				$youtube_code = get_field('youtube_code');
+
+				if ($youtube_code) {
+					$youtube_embed = 'https://www.youtube.com/embed/';
+					$youtube_url = $youtube_embed . $youtube_code;
+					$iframe_begin = '<div class="embed-container"><iframe src="';
+					$iframe_end = '" frameborder="0" allowfullscreen></iframe></div>';
+					$iframe = $iframe_begin . $youtube_url . $iframe_end;
+					echo $iframe;
+				}
+			?>
+
+			<p><?php the_field('gallery'); ?></p>
+
 		</div>
-		<div class="grid-cell sm-grid-1-1 md-grid-1-2">
-			<p>
-				<?php the_content(); ?>
-			</p>
-		</div>
+
+		<footer class="entry-footer">
+			<!--?php marvy_entry_list_footer(); ?-->
+		</footer>
 
 		<?php
 		wp_link_pages( array(
