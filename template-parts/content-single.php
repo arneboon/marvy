98644e4950b1eca49dfdfb005c28475356734a82
Arne Boon <arneboon@gmail.com>
2017-01-02 14:14:00 +0100

    project single page layout

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -13,42 +13,81 @@
 			<?php
 		}
 		?>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-		<h6>
-			<?php the_date('Y'); ?>
-			&nbsp;&nbsp;
-			<?php the_field('location'); ?>
-		</h6>
+		<center>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+
+			<!--ENTRY FOOTER-->
+			<footer class="entry-footer">
+				<!-- <?php marvy_entry_list_footer(); ?> -->
+				<?php
+					$date = get_the_date('Y');
+					echo '<p>'
+					. '<i class="ti-calendar"></i>'
+					. $date
+					. '</p>';
+
+					$client_name = get_field('client_name');
+					$client_website = esc_url( get_field('client_website') );
+					if ($client_name) {
+						if ($client_website) {
+							echo '<p>'
+							. '<i class="ti-id-badge"></i>'
+							. '<a href="'
+							. $client_website
+							. '" target="_blank">'
+							. $client_name
+							. '</a>'
+							. '</p>';
+						} else {
+							echo '<p>'
+							. '<i class="ti-id-badge"></i>'
+							. $client_name
+							. '</p>';
+						}
+					}
+
+					$location = get_field('location');
+					if ($location) {
+						echo '<p>'
+						. '<i class="ti-location-pin"></i>'
+						. $location
+						. '</p><br>';
+					}
+				?>
+			</footer>
+		</center>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content grid">
-		<div class="grid-cell">
+			<div class="grid-cell sm-grid-1-1 md-grid-1-1">
+				<!--INTRODUCTION-->
+				<span class="project-introduction ">
+					<p><?php the_field('introduction'); ?></p>
+				</span>
 
-			<p><?php the_field('introduction'); ?></p>
-			<p><?php the_content(); ?></p>
+				<!--CONTENT-->
+				<span class="project-content">
+					<?php the_content(); ?>
+				</span>
 
-			<?php
-				//https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
-				$youtube_code = get_field('youtube_code');
+				<!--YOUTUBE-->
+				<?php
+					//https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+					$youtube_code = get_field('youtube_code');
 
-				if ($youtube_code) {
-					$youtube_embed = 'https://www.youtube.com/embed/';
-					$youtube_url = $youtube_embed . $youtube_code;
-					$iframe_begin = '<div class="embed-container"><iframe src="';
-					$iframe_end = '" frameborder="0" allowfullscreen></iframe></div>';
-					$iframe = $iframe_begin . $youtube_url . $iframe_end;
-					echo $iframe;
-				}
-			?>
+					if ($youtube_code) {
+						$youtube_embed = 'https://www.youtube.com/embed/';
+						$youtube_url = $youtube_embed . $youtube_code;
+						$iframe_begin = '<div class="embed-container"><iframe src="';
+						$iframe_end = '" frameborder="0" allowfullscreen></iframe></div>';
+						$iframe = $iframe_begin . $youtube_url . $iframe_end;
+						echo $iframe;
+					}
+				?>
 
-			<p><?php the_field('gallery'); ?></p>
-
-		</div>
-
-		<footer class="entry-footer">
-			<!--?php marvy_entry_list_footer(); ?-->
-		</footer>
-
+				<!--GALLERY-->
+				<p><?php the_field('gallery'); ?></p>
+			</div>
 		<?php
 		wp_link_pages( array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'marvy' ),
